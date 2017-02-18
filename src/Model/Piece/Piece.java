ac158a08d356d09af1499790fe400dbbc230977c
Eriwyr <mail@v-chabrier.me>
2017-02-18 12:29:30 +0100

    add function in Piece

--- src/Model/Piece/Piece.java
+++ src/Model/Piece/Piece.java
@@ -7,4 +7,24 @@ import java.util.List;
 public class Piece {
     protected List<Position> shape;
     protected Position center;
+
+    public Piece(List<Position> shape, Position center) {
+        this.shape = shape;
+        this.center = center;
+    }
+
+    public void addBox(Position p){
+        //TODO add correct verification
+        shape.add(p);
+    }
+
+    public Boolean removeBox(Position p){
+        boolean ok;
+        ok = false;
+        if(shape.contains(p)){
+            shape.remove(p);
+            ok =true;
+        }
+        return ok;
+    }
 }
